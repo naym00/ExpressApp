0f20d37e65ec3135f76e203eccdb19f6948d711a
naym00 <naymhsain00@gmail.com>
2021-12-29 17:43:45 +0600

    First

---
+++ public/js/script.js
@@ -0,0 +1,124 @@
+function c_f_1()
+{
+    let str_from_btn = document.getElementById("btn-1").innerText;
+    if (str_from_btn.localeCompare("Convert to Fahrenheit") == 0)
+    {
+        let heigh_temperature = parseInt(document.getElementById("high-1").innerText.replace("oC", ""));
+        let low_temperature = parseInt(document.getElementById("low-1").innerText.replace("oC", ""));
+
+        let high_fahrenheit = (parseInt(((9*heigh_temperature)+160)/5)).toString();
+        let low_fahrenheit = (parseInt(((9*low_temperature)+160)/5)).toString();
+
+        document.getElementById("high-1").innerHTML = high_fahrenheit + "<sup>o</sup>F";
+        document.getElementById("low-1").innerHTML = low_fahrenheit + "<sup>o</sup>F";
+        document.getElementById("btn-1").innerHTML = "Convert to Celsius";
+    }
+    else if (str_from_btn.localeCompare("Convert to Celsius") == 0)
+    {
+        let heigh_temperature = parseInt(document.getElementById("high-1").innerText.replace("oF", ""));
+        let low_temperature = parseInt(document.getElementById("low-1").innerText.replace("oF", ""));
+
+        let high_celsius = (parseInt((5*(heigh_temperature - 32))/9)).toString();
+        let low_celsius = (parseInt((5*(low_temperature - 32))/9)).toString();
+
+        document.getElementById("high-1").innerHTML = high_celsius + "<sup>o</sup>C";
+        document.getElementById("low-1").innerHTML = low_celsius + "<sup>o</sup>C";
+        document.getElementById("btn-1").innerHTML = "Convert to Fahrenheit";
+    }
+}
+
+function c_f_2()
+{
+    let str_from_btn = document.getElementById("btn-2").innerText;
+    if (str_from_btn.localeCompare("Convert to Fahrenheit") == 0)
+    {
+        let heigh_temperature = parseInt(document.getElementById("high-2").innerText.replace("oC", ""));
+        let low_temperature = parseInt(document.getElementById("low-2").innerText.replace("oC", ""));
+
+        let high_fahrenheit = (parseInt(((9*heigh_temperature)+160)/5)).toString();
+        let low_fahrenheit = (parseInt(((9*low_temperature)+160)/5)).toString();
+
+        document.getElementById("high-2").innerHTML = high_fahrenheit + "<sup>o</sup>F";
+        document.getElementById("low-2").innerHTML = low_fahrenheit + "<sup>o</sup>F";
+        document.getElementById("btn-2").innerHTML = "Convert to Celsius";
+    }
+    else if (str_from_btn.localeCompare("Convert to Celsius") == 0)
+    {
+        let heigh_temperature = parseInt(document.getElementById("high-2").innerText.replace("oF", ""));
+        let low_temperature = parseInt(document.getElementById("low-2").innerText.replace("oF", ""));
+
+        let high_celsius = (parseInt((5*(heigh_temperature - 32))/9)).toString();
+        let low_celsius = (parseInt((5*(low_temperature - 32))/9)).toString();
+
+        document.getElementById("high-2").innerHTML = high_celsius + "<sup>o</sup>C";
+        document.getElementById("low-2").innerHTML = low_celsius + "<sup>o</sup>C";
+        document.getElementById("btn-2").innerHTML = "Convert to Fahrenheit";
+    }
+}
+
+function c_f_3()
+{
+    let str_from_btn = document.getElementById("btn-3").innerText;
+    if (str_from_btn.localeCompare("Convert to Fahrenheit") == 0)
+    {
+        let heigh_temperature = parseInt(document.getElementById("high-3").innerText.replace("oC", ""));
+        let low_temperature = parseInt(document.getElementById("low-3").innerText.replace("oC", ""));
+
+        let high_fahrenheit = (parseInt(((9*heigh_temperature)+160)/5)).toString();
+        let low_fahrenheit = (parseInt(((9*low_temperature)+160)/5)).toString();
+
+        document.getElementById("high-3").innerHTML = high_fahrenheit + "<sup>o</sup>F";
+        document.getElementById("low-3").innerHTML = low_fahrenheit + "<sup>o</sup>F";
+        document.getElementById("btn-3").innerHTML = "Convert to Celsius";
+    }
+    else if (str_from_btn.localeCompare("Convert to Celsius") == 0)
+    {
+        let heigh_temperature = parseInt(document.getElementById("high-3").innerText.replace("oF", ""));
+        let low_temperature = parseInt(document.getElementById("low-3").innerText.replace("oF", ""));
+
+        let high_celsius = (parseInt((5*(heigh_temperature - 32))/9)).toString();
+        let low_celsius = (parseInt((5*(low_temperature - 32))/9)).toString();
+
+        document.getElementById("high-3").innerHTML = high_celsius + "<sup>o</sup>C";
+        document.getElementById("low-3").innerHTML = low_celsius + "<sup>o</sup>C";
+        document.getElementById("btn-3").innerHTML = "Convert to Fahrenheit";
+    }
+}
+
+function c_f_4()
+{
+    let str_from_btn = document.getElementById("btn-4").innerText;
+    if (str_from_btn.localeCompare("Convert to Fahrenheit") == 0)
+    {
+        let heigh_temperature = parseInt(document.getElementById("high-4").innerText.replace("oC", ""));
+        let low_temperature = parseInt(document.getElementById("low-4").innerText.replace("oC", ""));
+
+        let high_fahrenheit = (parseInt(((9*heigh_temperature)+160)/5)).toString();
+        let low_fahrenheit = (parseInt(((9*low_temperature)+160)/5)).toString();
+
+        document.getElementById("high-4").innerHTML = high_fahrenheit + "<sup>o</sup>F";
+        document.getElementById("low-4").innerHTML = low_fahrenheit + "<sup>o</sup>F";
+        document.getElementById("btn-4").innerHTML = "Convert to Celsius";
+    }
+    else if (str_from_btn.localeCompare("Convert to Celsius") == 0)
+    {
+        let heigh_temperature = parseInt(document.getElementById("high-4").innerText.replace("oF", ""));
+        let low_temperature = parseInt(document.getElementById("low-4").innerText.replace("oF", ""));
+
+        let high_celsius = (parseInt((5*(heigh_temperature - 32))/9)).toString();
+        let low_celsius = (parseInt((5*(low_temperature - 32))/9)).toString();
+
+        document.getElementById("high-4").innerHTML = high_celsius + "<sup>o</sup>C";
+        document.getElementById("low-4").innerHTML = low_celsius + "<sup>o</sup>C";
+        document.getElementById("btn-4").innerHTML = "Convert to Fahrenheit";
+    }
+}
+
+function dataFromInput()
+{
+    let country = document.getElementById("country_input").value;
+    let city = document.getElementById("city_input").value;
+
+    console.log(country);
+    console.log(city);
+}
--- routes/index.js
+++ routes/index.js
@@ -37,41 +37,76 @@ var axios = require("axios").default;
 //     });
 // });
 
-router.get("/all/:country/:city", function (req, res, next) {
-  var country = req.params.country;
-  var city = req.params.city;
+router.get('/', function (req, res, next) {
+  res.render('new_index', { sss: 'Express' });
+});
+
+
+router.get("/all/bangladesh/dhaka", function (req, res, next) {
+
+  var country = "bangladesh";
+  var city = "dhaka";
   axios
-    .get(
-      "http://api.weatherapi.com/v1/current.json?key=24fd1c53215c4f9dbc195356212812&q=" +
-        country +
-        "&q=" +
-        city +
-        "&aqi=no"
-    )
+    .get("https://api.weatherapi.com/v1/history.json?key=1025c15bcdb04428906103946212912&q=" + country + "&q=" + city + "&dt=2021-12-27&end_dt=2021-12-28&aqi=yes")
     .then((response) => {
-      console.log(response.data);
-      console.log(response.data);
-      var jsonObject = JSON.stringify(response.data);
-      console.log(jsonObject);
-      var object = JSON.parse(jsonObject);
 
-      console.log(object.keys);
+      var da_ta = response.data;
+      var jsonObject = JSON.stringify(da_ta);
+      var jsObject = JSON.parse(jsonObject);
 
-      // for (x in object) {
-      //   res.render("index", { title: object[x].name, test: object[x].country });
-      //   // res.render("index", { title: object[x].country });
+      var all_keys = Object.keys(jsObject);
+
+      // for (const key in jsObject) {
+      //   console.log(key);
       // }
-      // for (x in object) {
-      res.render("index", { title: object.current.cloud });
-      //}
 
-      // res.render("index", { title: "" + Object.keys(object) });
+      // expected output:
+      // "a: 1"
+      // "b: 2"
+      // "c: 3"
+
+
+      console.log(all_keys);
+
     })
     .catch((error) => {
       console.log(error);
     });
 });
 
-// module.exports = app;
+
+
+// //router.get("/all/:country/:city", function (req, res, next) {
+
+
+//   router.get("/all/bangladesh/dhaka", function (req, res, next) {
+//     // var country = req.params.country;
+//     // var city = req.params.city;
+//     var country = "bangladesh";
+//     var city = "dhaka";
+//     axios
+//       .get("https://api.weatherapi.com/v1/history.json?key=1025c15bcdb04428906103946212912&q=" + country + "&q=" + city + "&dt=2021-12-27&end_dt=2021-12-28&aqi=yes")
+//       .then((response) => {
+//         console.log(response.data);
+//         // var jsonObject = JSON.stringify(response.data);
+//         // console.log(jsonObject);
+//         // var object = JSON.parse(jsonObject);
+
+//         // console.log(object.keys);
+
+//         // for (x in object) {
+//         //   res.render("index", { title: object[x].name, test: object[x].country });
+//         //   // res.render("index", { title: object[x].country });
+//         // }
+//         // for (x in object) {
+//         //    res.render("index", { title: object.current.cloud });
+//         //}
+
+//         // res.render("index", { title: "" + Object.keys(object) });
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   });
 
 module.exports = router;
